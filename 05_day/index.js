3edf5806b786b3f9097e276a61a3be7a6d85e2a6
hitisha-goyani <goyanihitisha@gmail.com>
2025-02-11 17:47:38 +0530

    day-5

---
+++ 05_day/index.js
@@ -0,0 +1,66 @@
+// function main(n){
+    //     console.log("again");
+    
+    //   return submain(n);
+    // }
+    
+    
+    // function submain(n){
+    //     return "submain gain " + n
+    // }
+    
+    // let num = 20;
+    // let fun =  main(num);
+    
+    // console.log(fun)
+    
+    
+    
+    // function rec(n){
+    
+    // if(n<1)
+    // {
+    //     return 1;
+    // }
+    // console.log(n)
+    
+    //     return rec(n-1)
+    // }
+    
+    
+    // function rec(n){
+    
+    // if(n<1)
+    // {
+    //     return 1;
+    // }
+    // console.log(n)
+    
+    //     return rec(n-1)
+    // }
+    
+    
+    //  let rc = rec(4)
+    
+    //  console.log(rc)
+    
+    
+    
+    // lexical scope
+    
+    // function outer()
+    // {
+    //     let name = "fsd"
+    
+    //     function inner(){
+    //         console.log(name)
+      
+    //     }
+    
+    //    return inner(name);
+    // }   
+    
+    
+    
+    // outer();
+    
