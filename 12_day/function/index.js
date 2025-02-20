bde8dd86bc5deb36ecc3594bffd541319bd1e4d5
hitisha-goyani <goyanihitisha@gmail.com>
2025-02-20 16:35:33 +0530

    day12

---
+++ 12_day/function/index.js
@@ -0,0 +1,49 @@
+
+
+function first(num){
+
+    // console.log("first one")
+
+    console.log(num*num)
+}
+
+let val1= first(10)
+
+console.log(val1)
+
+
+function second(num){
+
+    // console.log("second one")
+
+    // return "second one"
+
+    return num*num;
+}
+
+let value = second(20); 
+
+console.log(try1())
+
+function try1(){
+
+    return  "checking1"
+}
+
+let try2 = function(){
+
+    return "checking2"
+}
+
+console.log(try2())
+
+
+let fun=(name)=>{ // declaration
+        console.log(`my name is ${name}`)
+        // return `my name is ${name}`
+}
+
+const tname = fun("hitisha");
+
+// const telname = `my name is hitisha`  // return
+// const telname = udefine // console
