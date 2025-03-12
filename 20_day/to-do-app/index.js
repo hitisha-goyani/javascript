f094874dbe686774d9b49fdbccb43113c7ee140e
hitisha-goyani <goyanihitisha@gmail.com>
2025-03-12 17:53:17 +0530

    day-24

--- 20_day/to-do-app/index.js
+++ 20_day/to-do-app/index.js
@@ -1,142 +1,230 @@
+
+
+
 let task = document.getElementById("task")
 let add = document.getElementById("add")
 let tbody = document.getElementById("tbody")
+let priority = document.getElementById("priority")
 
 
-add.addEventListener("click",addTask)
+add.addEventListener("click", addTask)
 
 
-let task_arr = [];
+let task_arr = JSON.parse(localStorage.getItem("task_arr")) || []
 
 
-function  addTask(){
+
+showTask(task_arr)
+function setLocal(ta){
+    localStorage.setItem("task_arr", JSON.stringify(ta))
+    showTask(task_arr)
+}
+
+
+
+function sorting(){
+  let newArr = task_arr.sort((a,b) => b.pri - a.pri)
+
+ setLocal(newArr)
+    
+}
+
+
+let a= 10;
+let b = 20;
+
+b = b-a // 20 - 10 == 10
+a = b = 20
+
+
+
+priority.addEventListener("change", function(e){
+    console.log(e.target.value)
+})
+
+function addTask(){
 
     let obj = {
-
         id: Math.round(Math.random()*1000),
+        pri: priority.value,
         text : task.value,
         status : false
-
     }
-    if(task.name==""){
-       
-        task_arr.push(obj)
+if(task.name=="")
+{
+    task_arr.push(obj)
+}
+else{
+  task_arr =  task_arr.map((ele) => {
+        if(ele.id == task.name)
+        {
+            ele.text = task.value;
+            ele.pri = priority.value;
+        }
+        return ele;
+    })
+}
+    
 
-    }
-    else{
-        task_arr = task_arr.map((ele)=>{
+showTask(task_arr)
+ task.name=""
 
-            if(ele.id == task.name)
-                {
-                    ele.text = task.value;
-            }
-            return ele;
-        })
-    }
-
-   
-
-    showTask(task_arr)
-    task.name=""
 
 }
 
-// change task started//
+/////////// change status started ////////////////////
 
-function changeStatus(e){ 
-    
-    let new_task_arr = task_arr.map((ele) =>{
-        if(ele.text == e){
-           ele.status = !ele.status
-        }
-        return ele
+function changeStatus(e){
+    task_arr = task_arr.map((ele) => {
+            if(ele.id == e)
+            {     
+                ele.status = !ele.status
+            }
+            return ele     
     })
 
-    showTask(new_task_arr)
-
-}
-// change task ended//
-
-
-
-
-// delete task started //
-
-function taskdelete(e){
-
-    task_arr = task_arr.filter((ele)=>ele.id !=e)
-
-    showTask(task_arr)
+    setLocal(task_arr)
 }
 
-//delete task ended //
+/////////// change status ended ////////////////////
 
 
 
-// edit task started
+/////////// Task delete started ////////////////////
 
-function taskedit(e){
+function deleteTask(e){
+   task_arr =  task_arr.filter((ele) => ele.id != e)
 
-    console.log(e);
-    task.value = e.text;
-    task.name = e.id
+   setLocal(task_arr)
 }
 
-//edit task ended
+
+/////////// Task delete ended ////////////////////
+
+
+
+
+
+
+/////////// Task edit started ////////////////////
+
+
+    function editTask(e)
+    {
+        console.log(e)
+        task.value = e.text;
+        task.name=e.id
+    }
+
+/////////// Task edit ended ////////////////////
+
+
+
+
+
 
 
 function showTask(array){
 
     tbody.innerHTML = ""
-    array.map((ele)=>{
+    array.map((ele) => { // sleep
 
-        let tr = document.createElement("tr");
-        let td_text = document.createElement("td"); 
-        let td_status = document.createElement("td"); 
-        let td_edit = document.createElement("td"); 
-        let td_delete = document.createElement("td");
-        let btn_edit = document.createElement("button"); 
-        let btn_delete = document.createElement("button"); 
-        // let checkbox = document.createElement("input");
+
+
+           let tr = document.createElement("tr");
+            let td_text = document.createElement("td"); 
+            let td_status = document.createElement("td"); 
+            let td_edit = document.createElement("td"); 
+            let td_delete = document.createElement("td"); 
+            let btn_edit = document.createElement("button"); 
+            let btn_delete = document.createElement("button"); 
+            let span = document.createElement("span");
+
+
+
+            td_text.textContent = ele.text;
+            td_text.className="position-relative"
+           
+            span.textContent = ele.pri=="1" ? "High" : ele.pri=="2" ? "Mid" : "Low";
+            span.className = ele.pri == "3" 
+                                            ? "badge text-bg-secondary position-absolute top-0 start-0 translate-middle"
+                                            : ele.pri == "2" ?
+                                                "badge text-bg-warning position-absolute top-0 start-0 translate-middle"
+                                                : 
+                                                "badge text-bg-danger position-absolute top-0 start-0 translate-middle"
+            btn_edit.innerHTML=  `<i class="ri-edit-2-fill"></i>`
+            btn_edit.setAttribute("class","btn btn-primary btn-sm")
+            btn_delete.innerHTML=  `<i class="ri-delete-bin-5-line"></i>`
+            btn_delete.setAttribute("class", "btn btn-danger btn-sm")
+
+            td_status.textContent = ele.status ? " ✖️not completed" : "✔️completed";
+
+            td_text.append(span)
+            td_status.onclick = () => changeStatus(ele.id)
+            btn_delete.onclick = () => deleteTask(ele.id);
+            btn_edit.onclick = () => editTask(ele)
         
-        td_text.textContent = ele.text;
-        td_status.textContent = ele.status ? "✔️ completed" :"✖️ not completed";
-
-        // // Create the checkbox and add it to the status cell
-        // checkbox.type = "checkbox";
-        // checkbox.checked = ele.status; //Reflect task status (checked if completed)
-        // checkbox.classList.add("form-check-input");
-        // checkbox.onclick = () => changeStatus(ele.id); // Change status on click
-
-        td_status.onclick = () => changeStatus(ele.id)
-        btn_delete.onclick = () => taskdelete(ele.id)                                   
-        btn_edit.onclick = () => taskedit(ele)
-
-        
-       
 
 
-        btn_edit.innerHTML=  `<i class="ri-edit-2-fill"></i>`
-        btn_edit.setAttribute("class","btn btn-primary btn-sm")
-        btn_delete.innerHTML=  `<i class="ri-delete-bin-5-line"></i>`
-        btn_delete.setAttribute("class", "btn btn-danger btn-sm")
+            td_edit.append(btn_edit)
+            td_delete.append(btn_delete)
+             tr.append(td_text, td_status, td_edit, td_delete)
 
-        // td_status.append(checkbox);
-        td_edit.append(btn_edit)
-        td_delete.append(btn_delete)
-
-        tr.append(td_text, td_status, td_edit, td_delete)
-
-         if(ele.status)
-         {
-             tr.setAttribute("class", 'table-success')
-         }
-         else{
-            tr.setAttribute("class", 'table-warning')
-         }
-         
-    tbody.append(tr)  
-    task.value=""
-
-    })
+             if(ele.status)
+             {
+                 tr.setAttribute("class", 'table-success text-decoration-line-through');
+             }
+             else{
+                tr.setAttribute("class", 'table-warning')
+             }
+             
+        tbody.append(tr)   
+        task.value=""      
+   })
 }
+
+
+
+
+
+
+// btn_edit.innerHTML=  `<i class="ri-edit-2-fill"></i>`
+// btn_edit.setAttribute("class","btn btn-primary btn-sm")
+// btn_delete.innerHTML=  `<i class="ri-delete-bin-5-line"></i>`
+// btn_delete.setAttribute("class", "btn btn-danger btn-sm")
+
+
+//  showTask(task_arr)
+//     task.name=""
+
+// function  addTask(){
+
+//     let obj = {
+
+//         id: Math.round(Math.random()*1000),
+//         text : task.value,
+//         status : false
+
+//     }
+//     if(task.name==""){
+       
+//         task_arr.push(obj)
+
+//     }
+//     else{
+//         task_arr = task_arr.map((ele)=>{
+
+//             if(ele.id == task.name)
+//                 {
+//                     ele.text = task.value;
+//             }
+//             return ele;
+//         })
+//     }
+
+   
+
+//     showTask(task_arr)
+//     task.name=""
+
+// }
