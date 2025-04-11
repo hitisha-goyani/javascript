70286288cf520187599852d53e232098aebd8483
unknown <hitisha-goyani>
2025-04-11 22:20:25 +0530

    ad

--- 30_day/data_fetch/index.js
+++ 30_day/data_fetch/index.js
@@ -2,3 +2,6 @@ fetch('https://fakestoreapi.com/products/1')
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error('Error:', error));
+    
+
+
